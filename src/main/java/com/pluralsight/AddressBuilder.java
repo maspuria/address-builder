a3a2ba1b4b81fbc07b70d553c4df7f3d15821b27
mariannaaspuria <aspuria.mp@gmail.com>
2025-09-30 23:13:30 -0500

    scanner, stringbuilder, full name commit

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AddressBuilder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //create a StringBuilder object
+        StringBuilder skills = new StringBuilder();
+
+        System.out.println("Please provide the following information: ");
+        System.out.println("Full name: ");
+        String fullName = scanner.nextLine().trim();
+
+
+
+
+    }
 }
